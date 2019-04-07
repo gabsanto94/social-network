0f538ee3411510430190d864a8fa566ccfa3e274
vn504u6 <gabriel.santos@walmart.com>
2019-04-06 23:44:33 -0500

    added route for post. Checks to see if email is found, if not then reply with User not found. Also, I am using bcrypt to compare current password with the hash password we have in db.

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,8 +51,34 @@ router.post('/register', (req, res) => {
               });
           }
         })
+});
 
+// @route   GET api/users/login
+// @desc    Login user / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    // Find the user by email
+    User.findOne({ email })
+        .then( user => {
+            // check user
+            if(!user){
+                return res.status(404).json({email: "User not found."});
+            }
+
+            // check password - plain pwd
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        // TODO: generate token
+                        res.json({msg: "Success"})
+                    } else {
+                        return res.status(400).json({password: "Password incorrect"});
+                    }
+                })
+        })
 });
 
 module.exports = router;
